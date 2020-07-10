8932ba5a16489dd681c72b9e605b052631d11165
claudiocirne <claudiocirne65@gmail.com>
2020-07-10 12:21:34 -0300

    Primeiro commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul>
+      <li v-for="foto de fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+    data() {
+
+      return {
+        titulo:'Alurapic',
+        foto: []
+      }
+    },
+
+    created() {
+
+        alert('Criei o componente');
+    }
+}
+</script>
+
+<style>
+
+</style>
